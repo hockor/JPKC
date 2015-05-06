7a51aefe9f939a206e79200e2d4e7b76e84b5805
sywzh <821308407@qq.com>
2015-05-06 10:26:09 +0800

    asa

--- hockor/Content/admin.py
+++ hockor/Content/admin.py
@@ -2,14 +2,31 @@
 #-*-coding:utf8-*-
 
 from django.contrib import admin
-from Content.models import UploadFile,Announcement,New
+from Content.models import UploadFile,Announcement,New,Paper,Exam
 
-class Announcemment(admin.ModelAdmin):
-	list_display = ('name','content','time')
+class AnnouncemmentAdmin(admin.ModelAdmin):
+	list_display = ['name','content','time']
 
 class NewAdmin(admin.ModelAdmin):
-	list_display =  ('name','content','time')
+	list_display =  ['name','content','time']
+
+
+
+class ExamAdmin(admin.ModelAdmin):
+	list_display = ['question','optionA','optionB','optionC','optionD','answer','score']
+	list_display_links = ['question']
+	search_fields = ['question']
+
+class PaperAdmin(admin.ModelAdmin):
+	date_hierarchy = 'time'
+	list_display = ['exam','person','totalscore','time']
+	ordering = ['-id']
+	list_display_links = ['exam']
+	search_fields = ['exam','user__username','person']
+
 
 admin.site.register(UploadFile)
-admin.site.register(Announcement)
-admin.site.register(New)
+admin.site.register(Announcement,AnnouncemmentAdmin)
+admin.site.register(New,NewAdmin)
+admin.site.register(Paper,PaperAdmin)
+admin.site.register(Exam,ExamAdmin)
